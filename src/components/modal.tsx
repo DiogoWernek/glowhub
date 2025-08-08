e4cb264187223e27abe639ab2124ee8f90860a47
DiogoWernek <diogowernek86@gmail.com>
2025-08-08 00:41:15 -0300

    Finalizado modal e routes

---
+++ src/components/modal.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from "react";
+
+type ModalProps = {
+  isOpen: boolean; // indica se o modal está aberto ou fechado
+  onClose: () => void; // função chamada para fechar o modal
+  children: React.ReactNode; // conteúdo do modal, que pode ser qualquer elemento React
+};
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // Adiciona um listener de evento para fechar o modal quando clicar fora dele
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  // Se o modal não estiver aberto, retorna null para não renderizar nada
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-2">
+      <div
+        ref={modalRef}
+        className="bg-white rounded-lg shadow-lg max-w-md w-full relative p-6"
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-3 right-3 text-gray-500 hover:text-gray-800 cursor-pointer"
+        >
+          ✕
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,26 @@
 // Importações de imagens e ícones
-import { KeyIcon, UserIcon } from "@phosphor-icons/react";
+import { EnvelopeIcon, KeyIcon, UserIcon } from "@phosphor-icons/react";
 import Logo from "../assets/icons/glowhub_logo.svg";
 
 // Componentes
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Modal } from "../components/modal";
+import { useState } from "react";
 
 export const Login = () => {
+  const [showModalPassword, setShowModalPassword] = useState(false);
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background px-[2rem]">
+    <div className="flex items-center justify-center min-h-screen bg-background px-[1rem]">
       {/* Container principal do login */}
-      <div className="flex flex-col text-center border-2 border-border-primary rounded-[0.5rem] p-6 py-12 bg-white gap-5 shadow-lg w-full max-w-[22rem]">
+      <div className="flex flex-col text-center border-2 border-border-primary rounded-[0.5rem] p-6 py-12 pb-4 bg-white gap-5 shadow-lg w-full max-w-[22rem]">
         <div className="flex flex-col items-center gap-0.5">
           <img src={Logo} alt="Logo GlowHub" className="w-20 mb-2" />
           <h1 className="text-text-primary text-2xl font-bold">Glowhub</h1>
 
           <p className="text-text-secondary text-[.875rem]">
-            Acesse sua conta para continuar
+            Central de serviços estéticos
           </p>
         </div>
 
@@ -27,7 +31,6 @@ export const Login = () => {
           <Input
             label="Senha"
             type="password"
-            hasError={{ status: true, message: "Ta tendo erro aqui pai" }}
           >
             <KeyIcon size={18} />
           </Input>
@@ -42,7 +45,35 @@ export const Login = () => {
             Criar uma nova conta
           </a>
         </div>
+
+        <button
+          onClick={() => setShowModalPassword(true)}
+          className="text-text-secondary text-[.875rem] hover:text-text-secondary/80 active:text-text-secondary/80 cursor-pointer"
+        >
+          Esqueci minha senha
+        </button>
       </div>
+
+      <Modal
+        isOpen={showModalPassword}
+        onClose={() => {
+          setShowModalPassword(false);
+        }}
+      >
+        <div className="flex flex-col text-center gap-4">
+          <h2 className="text-text-primary text-xl font-bold">
+            Recuperar Senha
+          </h2>
+          <p className="text-text-secondary text-[.875rem]">
+            Insira seu e-mail cadastrado para receber as instruções de
+            recuperação de senha.
+          </p>
+          <Input label="E-mail" type="email">
+            <EnvelopeIcon size={18} />
+          </Input>
+          <Button children="Enviar Instruções" />
+        </div>
+      </Modal>
     </div>
   );
 };
